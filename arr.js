903b2b937679d2ea1b05ea46ddc3f58eee1e3f5d
Sajal Mittal <sajalmittalx@gmail.com>
2026-07-24 13:51:48 +0530

    array basic

---
+++ arr.js
@@ -0,0 +1,6 @@
+const a=[1.2,3,4,5];
+const b=[4,5];
+console.log("a=",a);
+console.log("b=",b);
+const c = [...a,...b];
+console.log("c=",c);
---
+++ arr1.js
@@ -0,0 +1,9 @@
+function sum(...num){
+    // const sum = num.reduce((i,s) => (s+i)) .  // New Method
+    sum=0;
+    for( i=0 ; i<num.length ; i++){
+        sum=sum+i;
+    }
+    return sum;
+}
+console.log(sum(1,2,3,4,5,6,7,8,9,10));

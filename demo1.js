8eb2f7a71d4210b096cf326debb31c6bba0f5cfe
Sajal Mittal <sajalmittalx@gmail.com>
2026-07-30 14:25:18 +0530

    demo

---
+++ demo1.js
@@ -0,0 +1,13 @@
+const student = {
+    name: "sajal",
+    age: 19,
+    branch: "CSE-AIML",
+}
+// const name =student.name ;
+// const age =student.age ;
+// const branch =student.branch ;
+const{name,age,branch} = student;
+
+console.log("name = ",name);
+console.log("age = ",age);
+console.log("branch = ",branch);

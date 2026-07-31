dc34529b90851f467804e554eef4935bf1ec5c12
Sajal Mittal <sajalmittalx@gmail.com>
2026-07-31 13:40:29 +0530

    export import

---
+++ test.js
@@ -0,0 +1,8 @@
+import {sum,add} from "./demo2.js";
+console.log("SUM = " , sum(1));
+console.log("SUM = " , sum(1,2,3,4,5));
+console.log("SUM = " , sum(13,132,23,4,3));
+console.log("\n")
+console.log("ADD = " , add(1));
+console.log("ADD = " , add(1,2,3,4,5));
+console.log("ADD = " , add(13,132,23,4,3));
